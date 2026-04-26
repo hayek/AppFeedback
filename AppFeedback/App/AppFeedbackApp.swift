c8f067d5c8f72f0ee6cb96614961c5797ef3ac95
Amir Hayek <free4sale@gmail.com>
2026-04-26 07:35:18 +0300

    feat: scaffold AppFeedback Xcode project

---
+++ AppFeedback/App/AppFeedbackApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct AppFeedbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("AppFeedback")
+        }
+    }
+}
---
+++ AppFeedbackTests/AppFeedbackTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import AppFeedback
+
+final class AppFeedbackTests: XCTestCase {
+    func test_placeholder() {
+        XCTAssertTrue(true)
+    }
+}
